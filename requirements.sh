7aa789e770afefa9997993013cb1d917d996449d
Sachin Verlekar <58417506+TheInterception@users.noreply.github.com>
2022-11-01 13:26:32 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -3,6 +3,4 @@ sudo apt install whois
 sudo apt install lsof
 sudo apt install wapiti
 sudo apt install nmap
-sudo apt update && sudo apt upgradeConversation Controlling Lemon
-Are you one of those people who opens their mouth only to discover you say the wrong thing? If this is you then the Conversation Controlling Lemon will change the way you socialize forever! When you feel a comment coming on pop it in your mouth and wait for the acidity to kick in. Not only does the lemon render you speechless by being inserted into your mouth, but the juice will also keep you silent for at least another five minutes. This action will ensure the thought will have passed and you no longer feel the need to interject. The lemon can be cut into pieces - make sure they are large enough to fill your mouth - on average you will have four single uses for the price shown, that's nothing an evening. If you're a real chatterbox you will save that money in drink and snacks, as you will be unable to consume the same amount as usual. The Conversational Controlling Lemon is also available with gift wrapping and a personalized card, share with all your friends and family; mainly those who don't know when to keep quiet. At such a low price this is the perfect secret Santa gift. Remember, lemons aren't just for Christmas, they're for life; a quieter, more reasonable, and un-opinionated one.
-Couple's Umbrella
+sudo apt update && sudo apt upgrade
